4cfc8a2a881ef1dd4e306fa03c484beee38d7f83
abarrientosrivas <alexisbarrientos97@gmail.com>
2024-01-09 14:16:36 -0300

    added assembly information

---
+++ SAPeador/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SAPeador")]
+[assembly: AssemblyDescription("Librearía de manejo de cliente SAP GUI por scripting.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alumina Coast")]
+[assembly: AssemblyProduct("SAPeador")]
+[assembly: AssemblyCopyright("MIT License")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a72d570e-1f36-48fd-aab7-1334bb7e3c15")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
