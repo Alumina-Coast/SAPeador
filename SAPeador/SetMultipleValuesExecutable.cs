2f556821b2439b04a190e014a9a8b2ead937178b
abarrientosrivas <alexisbarrientos97@gmail.com>
2024-01-10 14:12:30 -0300

    added base for classes to work on

---
+++ SAPeador/SetMultipleValuesExecutable.cs
@@ -0,0 +1,59 @@
+﻿using SAPFEWSELib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAPeador
+{
+    public class SetMultipleValuesExecutable : IExecutable
+    {
+        private InteractionState state = InteractionState.NOT_EXECUTED;
+        private string message = string.Empty;
+        private bool interruptOnFailure;
+        public List<string> Values { get; set; }
+        public string ItemPath { get; set; }
+
+        public SetMultipleValuesExecutable(string itemPath, List<string> values)
+        {
+            ItemPath = itemPath;
+            Values = values;
+        }
+
+        public InteractionState GetState()
+        {
+            return state;
+        }
+
+        public void SetState(InteractionState value)
+        {
+            state = value;
+        }
+
+        public string GetMessage()
+        {
+            return message;
+        }
+
+        public void SetMessage(string value)
+        {
+            message = value;
+        }
+
+        public bool GetInterruptOnFailure()
+        {
+            return interruptOnFailure;
+        }
+
+        public void SetInterruptOnFailure(bool value)
+        {
+            interruptOnFailure = value;
+        }
+
+        void IExecutable.Execute(GuiSession session)
+        {
+
+        }
+    }
+}
